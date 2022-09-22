210edcb670ec64ec28f1b7bac653852d853de067
Fadli Rizaldy <85619226+fadlirizaldy@users.noreply.github.com>
2022-09-22 20:48:59 +0700

    Update app.py

--- Project_Loan_Prediction/app.py
+++ Project_Loan_Prediction/app.py
@@ -65,7 +65,10 @@ salary = st.number_input('Input your current salary (per year)', 0)
 btn_pred = st.button('Request Loan')
 
 if btn_pred:
-    emp_stat = 1 if emp_status == 'Yes' else 0
+    if emp_status == "Yes":
+        emp_stat = 1 
+    else:
+        emp_stat = 0
     
     tmp_pd = pd.DataFrame({
         'Employed':[emp_stat],
